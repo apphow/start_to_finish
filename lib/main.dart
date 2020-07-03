fbc4b9cf95891e447794c62d18688cff8de4d6cc
apphow <apehow111@gmail.com>
2020-07-03 19:57:39 -0400

    reformat code

--- lib/main.dart
+++ lib/main.dart
@@ -10,24 +10,27 @@ class MyPassport extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text('Passport Approval Page'),
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text('Passport Approval Page'),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 50.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                      radius: 120.0,
+                      backgroundImage: AssetImage('images/mesjpeg.jpeg')),
+                ],
+              ),
+            ),
           ),
-          body: SafeArea(
-              child: Center(
-                  child: Padding(
-                      padding: EdgeInsets.only(top: 50.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircleAvatar(
-                              radius: 120.0,
-                              backgroundImage:
-                                  AssetImage('images/mesjpeg.jpeg')),
-                        ],
-                      ))))),
+        ),
+      ),
     );
   }
 }
